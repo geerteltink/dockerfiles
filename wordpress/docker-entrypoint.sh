ab86357dcbf967c6802ce2838db98762f7ec4787
Geert Eltink <xtreamwayz@gmail.com>
2016-07-29 21:39:02 +0200

    Update WordPress container

--- wordpress/docker-entrypoint.sh
+++ wordpress/docker-entrypoint.sh
@@ -8,5 +8,14 @@ if ! [ -e /app/public/index.php -a -e /app/public/wp-includes/version.php ]; the
     tar -xzf wordpress.tar.gz -C /tmp/
     echo "Copying wordpress..."
     cp -a /tmp/wordpress/. /app/public/
-    echo "Job's done!"
+    echo "Setting permissions..."
+    chown -R www-data:www-data /app
 fi
+
+if [ ! -e wp-config.php ]; then
+    echo "WordPress config not found, creating one for you..."
+    cp -a /app/wp-config.php /app/public/wp-config.php
+    chown www-data:www-data /app/public/wp-config.php
+fi
+
+echo "Job's done!"
